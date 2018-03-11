c7813a7f15e91657920e4768feef985140dceca5
Imdad Ali <imdad@bluelead.co.uk>
2018-03-11 06:57:04 -0400

    Basic Project with AngularJS
    Nothing complicated, kept things simple with the project structure and
    technology.

---
+++ COMP4203/COMP4203.Web/App_Start/BundleConfig.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Optimization;
+
+namespace COMP4203.Web
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles)
+		{
+			const string ANGULAR_APP_ROOT = "~/Scripts/";
+			const string ANGULAR_VIRTUAL_BUNDLE_PATH = ANGULAR_APP_ROOT + "angularjs";
+
+			var angularScriptBundle = new ScriptBundle(ANGULAR_VIRTUAL_BUNDLE_PATH)
+				.Include(ANGULAR_APP_ROOT + "angular.js")
+				.Include(ANGULAR_APP_ROOT + "angular-route.js")
+				.Include(ANGULAR_APP_ROOT + "app.js")
+				.IncludeDirectory(ANGULAR_APP_ROOT + "Controllers", "*.js", searchSubdirectories: false);
+
+			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+						"~/Scripts/jquery-{version}.js"));
+
+			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+						"~/Scripts/jquery.validate*"));
+
+			bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+						"~/Scripts/modernizr-*"));
+
+			bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+					  "~/Scripts/bootstrap.js",
+					  "~/Scripts/respond.js"));
+
+			bundles.Add(angularScriptBundle);
+
+			bundles.Add(new StyleBundle("~/Content/css").Include(
+					  "~/Content/bootstrap.css",
+					  "~/Content/site.css"));
+
+			BundleTable.EnableOptimizations = false;
+		}
+	}
+}
+ 
---
+++ COMP4203/COMP4203.Web/Scripts/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('app', ['ngRoute']);
+
+app.config(function ($routeProvider) {
+    $routeProvider.
+        when('/home', {
+            templateUrl: 'Home/Index'
+        }).
+        otherwise({
+            redirectTo: 'Home/Index'
+        });
+});
