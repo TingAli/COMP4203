7f1d98edc294883429786e4d2f94d2a1c42be419
BradleyKoon <bradleykoon@outlook.com>
2018-03-27 22:34:09 -0400

    Add files via upload

---
+++ SimulationEnvironment/SimulationEnvironment/Message.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulationProtocols
+{
+    class Message
+    {
+        static int messageCount = 0;
+        private int messageID;
+        private MobileNode sourceNode, destinationNode;
+        private double msgSpeed = 0;
+        public Message(MobileNode src, MobileNode dst)
+        {
+            messageID = ++messageCount;
+            sourceNode = src;
+            destinationNode = dst;
+        }
+
+        public MobileNode GetSourceNode() { return sourceNode; }
+        public MobileNode GetDestinstationNode() { return destinationNode; }
+        public int GetMessageID() { return messageID; }
+        public double GetMsgSpeed() { return msgSpeed; }
+        public void calcMsgSpeed(MobileNode src, MobileNode dst)
+        {
+            Random rand = new Random();
+            int time = rand.Next(50, 101);
+            msgSpeed = src.GetDistance(dst) / time;
+        }
+
+        public void Print() => Console.WriteLine("Message from Node {0} to Node {1}.", sourceNode.GetNodeID(), destinationNode.GetNodeID());
+    }
+}
---
+++ SimulationEnvironment/SimulationEnvironment/RoutingPacket.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimulationProtocols
+{
+    class RoutingPacket
+    {
+        private List<MobileNode> nodeRoute;
+
+        public RoutingPacket() => nodeRoute = new List<MobileNode>();
+
+        public List<MobileNode> GetNodeRoute() => nodeRoute;
+
+        public RoutingPacket Copy()
+        {
+            RoutingPacket packet = new RoutingPacket();
+            foreach (MobileNode node in nodeRoute)
+            {
+                packet.AddNodeToRoute(node);
+            }
+            return packet;
+        }
+
+        public void AddNodeToRoute(MobileNode node)
+        {
+            nodeRoute.Add(node);
+        }
+
+        public void AddNodesToRoute(List<MobileNode> nodes)
+        {
+            nodeRoute.AddRange(nodes);
+        }
+
+        public bool IsInRouteAlready(MobileNode node)
+        {
+            return nodeRoute.Contains(node);
+        }
+
+        public bool RouteCompare(RoutingPacket route)
+        {
+            if (nodeRoute.Count != route.GetNodeRoute().Count)
+            {
+                return false;
+            }
+            else
+            {
+                for (int i = 0; i < nodeRoute.Count; i++)
+                {
+                    if (!nodeRoute[i].Equals(route.GetNodeRoute()[i]))
+                    {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
