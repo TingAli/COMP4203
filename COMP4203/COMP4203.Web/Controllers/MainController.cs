3e5e6a8c9d8b3a0455bd3b77f380eb339132c2b8
James Tyler <jet.48@outlook.com>
2018-03-31 03:08:10 -0400

    #21 - Ammendment 1

--- COMP4203/COMP4203.Web/Controllers/MainController.cs
+++ COMP4203/COMP4203.Web/Controllers/MainController.cs
@@ -51,44 +51,5 @@ namespace COMP4203.Web.Controllers
             sim.RunSimulation();
             
         }
-
-	    [HttpGet, Route("api/testtwo/gettesttwolist")]
-	    public void GetTestTwoList([FromUri] int aNumber)
-	    {
-		    List<Test> testTwoList = new List<Test>
-		    {
-			    new Test
-			    {
-				    Id = Guid.NewGuid(),
-				    Name = $"Name {aNumber + 10}",
-				    Number = aNumber + 10
-			    },
-			    new Test
-			    {
-				    Id = Guid.NewGuid(),
-				    Name = $"Name {aNumber + 20}",
-				    Number = aNumber + 20
-			    },
-			    new Test
-			    {
-				    Id = Guid.NewGuid(),
-				    Name = $"Name {aNumber + 30}",
-				    Number = aNumber + 30
-			    }
-		    };
-
-		    Hub.Clients.All.broadcastTests(JsonConvert.SerializeObject(testTwoList));
-	    }
-
-	    [HttpGet, Route("api/main/testdebug")]
-	    public void TestDebug()
-	    {
-		    Hub.Clients.All.broadcastOutputMessage(JsonConvert.SerializeObject(new OutputMessage
-		    {
-				Id = Guid.NewGuid(),
-				Tag = "Test",
-				Message = "Test message..."
-		    }));
-	    }
     }
 }
