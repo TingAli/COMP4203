c05759c2d37c71710faea8d3754bccaaca6553a8
James Tyler <jet.48@outlook.com>
2018-03-31 01:20:14 -0400

    c4

--- COMP4203/COMP4203.Web/Controllers/MainController.cs
+++ COMP4203/COMP4203.Web/Controllers/MainController.cs
@@ -13,11 +13,14 @@ namespace COMP4203.Web.Controllers
 	    public MainController()
 	    {
 
-	    }
+        }
 
-        [HttpGet, Route("api/main/run")]
-        public void RunTest([FromUri] int nodeNumber, [FromUri] int messageNumber,
-            [FromUri] int simSpeedNumber, [FromUri] int tabIndex)
+        [HttpGet, Route("api/main/run/{nodeNumber}/{messageNumber}/{simSpeedNumber}/{tabIndex}")]
+        public void RunTest(
+	        int nodeNumber,
+	        int messageNumber,
+	        int simSpeedNumber,
+	        int tabIndex)
         {
             bool demo = true;
             if (demo)
